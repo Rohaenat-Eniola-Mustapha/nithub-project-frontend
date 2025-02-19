5f748c67e15ecc62f155c57639aa7eb91b10ae93
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-02-19 12:48:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d605615d8de819381a5aa14688b8def2.js"
+  "/Rohaenat-Eniola-Mustapha/nithub-project-frontend/precache-manifest.db5785ea8b9a26b6fc8e84f39fecf65a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rohaenat-Eniola-Mustapha/nithub-project-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
